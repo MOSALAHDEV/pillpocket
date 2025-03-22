04f0acbcca948242b82ea2ac7a1e5cf31f5919b0
MOSALAHDEV <mosalah525446@gmail.com>
2025-03-22 00:41:24 -0700

    add

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.user import User
+from models.medication import Medication
+from models.order import Order
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
@@ -9,3 +13,14 @@ def status():
     Returns the status of the API.
     """
     return jsonify({'status': 'OK'})
+
+@app_views.route('/users', methods=['GET'], strict_slashes=False)
+def stats():
+    """
+    Returns the stats of the API.
+    """
+    return jsonify({
+        'users': storage.count(User),
+        'medications': storage.count(Medication),
+        'orders': storage.count(Order)
+    })
